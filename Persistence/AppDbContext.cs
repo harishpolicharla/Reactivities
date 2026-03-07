90170279890757fcb6d85a14344561034ec043cb
Harish Kumar <harish.policharla@outlook.com>
2026-03-07 22:43:47 +0800

    First Commit

---
+++ Persistence/AppDbContext.cs
@@ -0,0 +1,11 @@
+using System;
+using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence;
+
+public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+{
+    public DbSet<Activity> Activities { get; set; }
+
+}
